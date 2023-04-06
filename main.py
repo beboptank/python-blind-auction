1db1931ffdf87678aada608c5a8218985802a8b3
beboptank <beboptank12@gmail.com>
2023-04-06 14:44:58 -0400

    added line to assign new user input to other_bids variable at the end of while loop in get_user_bids()

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def get_user_bids(all_bids): #all_bids should be a dictionary
         user_name = input("What is your name?: ")
         bid = input("What's your bid?: $")
         all_bids.update({user_name: bid})
+        other_bids = input("Are there any other bidders? Type 'yes' or 'no'.\n")
 
 def blind_auction():
     display_art()
